95fa3ed441bdcafeade7ccd70d9d77df178ff392
Sahil <n.sahil2709@gmail.com>
2026-03-18 10:46:15 +0530

    created an projectapp called products to create an model to interact with the client side using the api folder

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,15 @@
+import json
 from django.http import JsonResponse
 
 def api_home(requests, *args, **kwargs):
-  return JsonResponse({"message" : "Hi There"})
+  body = requests.body
+  data = {}
+  try:
+    data = json.loads(body)
+  except:
+    pass
+  print(data)
+  data['params'] = dict(requests.GET)
+  data['headers'] = dict(requests.headers)
+  data['content_type'] = requests.content_type
+  return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.get(endpoint, params={"abc" : 123}, json={"query" : "Hello world"})
 # print(get_response.text)
 # print(get_response.status_code)
 print(get_response.json())
