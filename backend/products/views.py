5a6c66af3f3c1277470e776c150f81a152611e1b
Sahil <n.sahil2709@gmail.com>
2026-03-18 14:59:56 +0530

    feat: implement ProductMixinView using DRF mixins for list, retrieve, and create operations

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -36,6 +36,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
     instance = serializer.save()
     if not instance.content:
       instance.content = instance.title
+      instance.save()
 
 Product_Update_View = ProductUpdateAPIView.as_view()
 
@@ -51,6 +52,30 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
 Product_delete_View = ProductDestroyAPIView.as_view()
 
 
+
+class ProductMixinView(mixins.ListModelMixin,mixins.RetrieveModelMixin, generics.GenericAPIView, mixins.CreateModelMixin):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializers
+  lookup_field = 'pk'
+
+  def get(self, request, *args, **kwargs):
+    pk = kwargs.get("pk")
+    if pk is not None:
+      return self.retrieve(request, *args, **kwargs)
+    return self.list(request, *args, **kwargs)
+  
+  def post(self, request, * args, **kwargs):
+    return self.create(request, *args, **kwargs)
+  
+  def perform_create(self, serializer):
+    print(serializer.validated_data)
+    title = serializer.validated_data.get('title')
+    content = serializer.validated_data.get('content') or None
+    if content is None:
+      content = "this is single content"
+    serializer.save(content=content)
+
+product_mixin_view = ProductMixinView.as_view()
 
 
 
