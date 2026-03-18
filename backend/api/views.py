e534160948227f3d1e2817ede065f915e35715b7
Sahil <n.sahil2709@gmail.com>
2026-03-18 11:43:41 +0530

    feat: implement ProductSerializer with computed fields and update api view to use DRF Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,21 @@
 import json
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Product
+# from django.http import JsonResponse, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializers
 
+@api_view(["GET"])
 def api_home(requests, *args, **kwargs):
-  model_data = Product.objects.all().order_by("?").first()
+  """
+  DRF API View
+  """
+  
+  instance = Product.objects.all().order_by("?").first()
   data = {}
-  if model_data:
-    data['id'] = model_data.id
-    data['title'] = model_data.title
-    data['content'] = model_data.content
-    data['price'] = model_data.price
-  return JsonResponse(data)
+  if instance:
+    # data = model_to_dict(instance)
+    data = ProductSerializers(instance).data
+  return Response(data)
+  
