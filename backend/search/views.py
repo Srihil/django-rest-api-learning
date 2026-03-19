cd1861bba0ce2f93cdc70eb08b432f4f62fdb85e
Sahil <n.sahil2709@gmail.com>
2026-03-19 13:02:15 +0530

    Implemeneted search method to the project and now with algolia search mehtod

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,8 +1,18 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializers
+from . import client
 
-class SearchListView(generics.ListAPIView):
+class SearchListView(generics.GenericAPIView):
+  def get(self, request, *args, **kwargs):
+    query = request.GET.get('q')
+    if not query:
+      return Response('', status=400)
+    results = client.perform_search(query)
+    return Response(results)
+
+class SearchListOldView(generics.ListAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializers
 
